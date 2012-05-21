6e5aaa35895f801e32edb59fa4c8413769426f54
David Benjamin <davidben@mit.edu>
2012-05-20 21:09:20 -0400

    Tweak index.html hack

--- test_server.py
+++ test_server.py
@@ -22,23 +22,21 @@ def create_app():
     """
     kdc_app = kdc.create_app()
 
-    def _throw_not_found(e, s): raise NotFound()
-
     web_scripts = os.path.join(os.path.dirname(__file__), 'web_scripts')
-    app = DispatcherMiddleware(
-        SharedDataMiddleware(_throw_not_found, { '/': web_scripts, }),
-        { '/kdc': kdc_app, })
 
-    def _index_html_hack(environ, start_response):
+    def throw_not_found(e, s): raise NotFound()
+    static_app = SharedDataMiddleware(throw_not_found, { '/': web_scripts, })
+    def index_html_hack(environ, start_response):
         try:
-            return app(environ, start_response)
+            return static_app(environ, start_response)
         except NotFound, e:
             environ['PATH_INFO'] = environ.get('PATH_INFO', '') + '/index.html'
             try:
-                return app(environ, start_response)
+                return static_app(environ, start_response)
             except NotFound, e:
                 return e(environ, start_response)
-    return _index_html_hack
+
+    return DispatcherMiddleware(index_html_hack, { '/kdc': kdc_app, })
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
