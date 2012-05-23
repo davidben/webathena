ba0b77a290f2ff0963fe99c5920820495f912a33
Alan Huang <cesium@mit.edu>
2012-05-23 16:27:30 -0400

    More layout things and stuff.

--- web_scripts/js/ui.js
+++ web_scripts/js/ui.js
@@ -1,10 +1,15 @@
 "use strict";
 
 $(function() {
-    $('#whatis').click(function() {
+    $('#whatis a').click(function() {
         $('#info').slideToggle();
         return false;
     });
+    $('#logout button').click(function() {
+        window.location.reload();
+    });
+    
+    $('#authed').slideUp(0);
     
     $('#login').submit(function() {
         var username = this.username.value,
@@ -25,6 +30,7 @@ $(function() {
         if(fail)
             return false;
         
+        this.password.value = '';
         var text = $('#submit').text();
         $('#submit').attr('disabled', 'disabled').text('.');
         var interval = setInterval(function() {
@@ -38,6 +44,9 @@ $(function() {
             console.log(username);
             console.log(reply);
             reset();
+            $('#login').slideUp();
+            $('#authed').slideDown();
+            $('#principal').text(reply.cname.nameString + '@' + reply.crealm);
         }, function(error) {
             console.log("Error in AS_REQ: " + error); // TODO actual error reporting
             reset();
