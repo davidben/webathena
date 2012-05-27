d69db83bb75f6d455f2ee3eca966564cc08fdf06
David Benjamin <davidben@mit.edu>
2012-05-26 22:51:54 -0400

    Make the htaccess parser even more complicated
    At some point we may as well just launch Apache.

--- test_server.py
+++ test_server.py
@@ -1,42 +1,49 @@
 #!/usr/bin/env python
 
-# # Enter the virtualenv
-# import os.path
-# _activate = os.path.join(os.path.dirname(__file__),
-#                          'env/bin/activate_this.py')
-# execfile(_activate, dict(__file__=_activate))
-
 # Add our code to path.
 import os.path
 import re
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), 'kdc'))
+
+BASEDIR = os.path.dirname(__file__)
+sys.path.append(os.path.join(BASEDIR, 'kdc'))
 
 from werkzeug.exceptions import NotFound
 from werkzeug.wsgi import SharedDataMiddleware, DispatcherMiddleware
 
 import kdc
 
-HEADER_RE = re.compile(r'Header +add +([^ ]+) "([^"]+)"')
+HEADER_RE = re.compile(r'Header +(set|add|unset) +([^ ]+)( +"([^"]+)")?')
+FILES_RE = re.compile(r'<Files ([^>]+)>')
+FILES_CLOSE_RE = re.compile(r'</Files>')
 
 def parse_htaccess():
     """
     Parse some subset of .htaccess so we can serve the same headers.
     """
-    headers = {}
-    htaccess = os.path.join(os.path.dirname(__file__), 'web_scripts/.htaccess')
+    directives = []
+    htaccess = os.path.join(BASEDIR, 'web_scripts/.htaccess')
     with open(htaccess) as f:
         for line in f:
             line = line.strip()
-            # TODO: Support Header set, etc. Also we'll need <Files>
-            # and stuff later. We also don't handle escaping, but
-            # whatever.
             m = HEADER_RE.match(line)
             if m:
-                header = m.group(1)
-                value = m.group(2)
-                headers[header] = value
-    return headers
+                action = m.group(1)
+                header = m.group(2)
+                value = m.group(4)
+                if header != 'Strict-Transport-Security':
+                    directives.append(('Header', action, header, value))
+                continue
+            m = FILES_RE.match(line)
+            if m:
+                filename = m.group(1)
+                directives.append(('Files', filename))
+                continue
+            m = FILES_CLOSE_RE.match(line)
+            if m:
+                directives.append(('/Files',))
+                continue
+    return directives
 
 def create_app():
     """
@@ -44,7 +51,7 @@ def create_app():
     """
     kdc_app = kdc.create_app()
 
-    web_scripts = os.path.join(os.path.dirname(__file__), 'web_scripts')
+    web_scripts = os.path.join(BASEDIR, 'web_scripts')
 
     def with_index_html(environ, start_response):
         environ['PATH_INFO'] = environ.get('PATH_INFO', '') + '/index.html'
@@ -55,15 +62,27 @@ def create_app():
     return DispatcherMiddleware(static_app, { '/kdc': kdc_app, })
 
 def apply_htaccess(app):
-    htaccess_headers = parse_htaccess()
-    del htaccess_headers['Strict-Transport-Security']
+    directives = parse_htaccess()
 
     def wrapped(environ, start_response):
         def wrapped_start_response(status, headers):
-            headers = [(h, v) for (h, v) in headers
-                       if h not in htaccess_headers]
-            for key, value in htaccess_headers.items():
-                headers.append((key, value))
+            enabled = [True]
+            for directive in directives:
+                if directive[0] == 'Files':
+                    enabled.append(environ['PATH_INFO'] ==
+                                   os.path.join('/', directive[1]))
+                elif directive[0] == '/Files':
+                    enabled.pop()
+                elif enabled[-1]:
+                    if directive[0] == 'Header':
+                        _, action, header, value = directive
+                        if action in ('set', 'unset'):
+                            headers = [(k, v) for (k, v) in headers
+                                       if k != header]
+                        if action in ('add', 'set'):
+                            headers.append((header, value))
+                    else:
+                        raise ValueError(directive)
             start_response(status, headers)
         return app(environ, wrapped_start_response)
     return wrapped
