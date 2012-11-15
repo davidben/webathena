f42d5d2ed8c30dbbf4802bc67aafddc3d337d518
David Benjamin <davidben@mit.edu>
2012-11-15 13:19:52 -0500

    Actually fix the IE console thing
    Not we really work in IE right now due to lack of crypto.

--- web_scripts/js/util.js
+++ web_scripts/js/util.js
@@ -3,7 +3,7 @@ window.getRandomBytes = function(nBytes) {
     return sjcl.random.randomWords(Math.ceil(nBytes / 4));
 };
 
-function log() {
-    if (console && console.log)
-        console.log(arguments);
+function log(arg) {
+    if (typeof console != "undefined" && console.log)
+        console.log(arg);
 }
