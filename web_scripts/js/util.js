a975ac0af7d8f7da523fca20b32a7e0edc8168fd
David Benjamin <davidben@mit.edu>
2013-01-16 20:06:46 -0500

    Polyfill Function.prototype.bind
    Seriously...

--- web_scripts/js/util.js
+++ web_scripts/js/util.js
@@ -61,3 +61,29 @@ if (!window.btoa) {
         return sjcl.codec.base64.fromBits(sjcl.codec.byteString.toBits(b));
     };
 }
+
+// This is too useful to not polyfill.
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+        if (typeof this !== "function") {
+            // closest thing possible to the ECMAScript 5 internal
+            // IsCallable function
+            throw new TypeError("Not callable");
+        }
+
+        var aArgs = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP = function () {},
+            fBound = function () {
+                return fToBind.apply(this instanceof fNOP && oThis
+                                     ? this
+                                     : oThis,
+                                     aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+
+        return fBound;
+    };
+}
