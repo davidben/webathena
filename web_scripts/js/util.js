c2499ad07250dda05fb5e61c3edb864c3a92e4aa
David Benjamin <davidben@mit.edu>
2013-01-30 19:58:20 -0500

    Move sjcl_byteString to util.js
    Oops. Broke the atob/btoa polyfills.

--- web_scripts/js/util.js
+++ web_scripts/js/util.js
@@ -22,14 +22,49 @@ if (!Object.create) {
     };
 }
 
+// "Byte string" codec based on sjcl.codec.utf8String. It's pretty
+// bogus though. We should write a typed array polyfill (either only
+// supporting DataView or monkey-patching a non-standard
+// Uint8Array.prototype.get/set so it's not laughingly inefficient.
+var sjcl_byteString = {
+    /** Convert from a bitArray to a byte string. */
+    fromBits: function (arr) {
+        var out = "", bl = sjcl.bitArray.bitLength(arr), i, tmp;
+        for (i=0; i<bl/8; i++) {
+            if ((i&3) === 0) {
+                tmp = arr[i/4];
+            }
+            out += String.fromCharCode(tmp >>> 24);
+            tmp <<= 8;
+        }
+        return out;
+    },
+
+    /** Convert from a byte string to a bitArray. */
+    toBits: function (str) {
+        var out = [], i, tmp=0;
+        for (i=0; i<str.length; i++) {
+            tmp = tmp << 8 | str.charCodeAt(i);
+            if ((i&3) === 3) {
+                out.push(tmp);
+                tmp = 0;
+            }
+        }
+        if (i&3) {
+            out.push(sjcl.bitArray.partial(8*(i&3), tmp));
+        }
+        return out;
+    }
+};
+
 if (!window.atob) {
     window.atob = function(a) {
-        return sjcl.codec.byteString.fromBits(sjcl.codec.base64.toBits(a));
+        return sjcl_byteString.fromBits(sjcl.codec.base64.toBits(a));
     };
 }
 if (!window.btoa) {
     window.btoa = function(b) {
-        return sjcl.codec.base64.fromBits(sjcl.codec.byteString.toBits(b));
+        return sjcl.codec.base64.fromBits(sjcl_byteString.toBits(b));
     };
 }
 
